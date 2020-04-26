4cdd7dcc3f45e54dfd64058184c4ff7ac08d1253
Chris Penner <christopher.penner@gmail.com>
2020-04-26 12:06:00 -0600

    Parsing JSON from stdin

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,16 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support" ]
+, dependencies =
+  [ "aff"
+  , "argonaut"
+  , "console"
+  , "effect"
+  , "node-fs"
+  , "node-process"
+  , "node-streams"
+  , "psci-support"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,28 @@ module Main where
 
 import Prelude
 
+import Data.Argonaut (jsonParser, stringify)
+import Data.Either (Either(..))
+import Data.Show (show)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (Aff, launchAff_, makeAff, nonCanceler)
+import Effect.Class.Console as Console
+import Effect.Ref as Ref
+import Node.Encoding (Encoding(..))
+import Node.Process as Process
+import Node.Stream (onDataString, onEnd, onError)
+
+readStdIn :: Aff String
+readStdIn = makeAff \handler -> do
+  buffer <- Ref.new ""
+  onDataString Process.stdin UTF8 \chunk -> Ref.modify_ (_ <> chunk) buffer
+  onEnd Process.stdin (handler =<< Right <$> Ref.read buffer)
+  onError Process.stdin (handler <<< Left)
+  pure nonCanceler
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = launchAff_ do
+  input <- readStdIn
+  case jsonParser input of
+       Left err -> Console.log err
+       Right json -> Console.log $ stringify json
